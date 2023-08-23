2c2e9c48a8bef04b182e1bd55d1357254c71a5f6
gabrielsantos578 <santos.gabriel67018@gmail.com>
2023-08-23 16:25:23 -0300

    APT - Continuing the development of the Biblioteca project.

---
+++ 08 - August/Biblioteca/Biblioteca/Controllers/EmprestimoController.cs
@@ -0,0 +1,28 @@
+﻿using Biblioteca.Data;
+using Biblioteca.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Biblioteca.Controllers
+{
+    public class EmprestimoController : Controller
+    {
+
+        readonly private ApplicationDBContext _db;
+        public EmprestimoController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<EmprestimoModel> emprestimos = _db.Emprestimo;
+            return View(emprestimos);
+        }
+
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ 08 - August/Biblioteca/Biblioteca/Models/EmprestimoModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Biblioteca.Models
+{
+    public class EmprestimoModel
+    {
+
+        // Modelo do Empréstimo no Banco de Dados
+        public int Id { get; set; }
+        public string Recebedor { get; set; }
+        public string Fornecedor { get; set;}
+        public string Livro { get; set;}
+        public DateTime Data { get; set; } = DateTime.Now;
+
+}       
+}
